203fa3a2f92baa932c5c40a740e1fb4e156bec10
tejamodi <139538405+tejamodi@users.noreply.github.com>
2023-11-07 16:02:48 +0530

    Add files via upload

---
+++ src/pages/Meal/MealType.jsx
@@ -0,0 +1,86 @@
+import React, { useContext, useEffect, useState } from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import { store } from '../../App'
+import Filters from '../../components/Filters/Filters'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+const MealType = () => {
+    const { mealName, formData, setFormData, setNavid } = useContext(store)
+    const [filteredData, setFilteredData] = useState([])
+    const [filteredFullData, setFilteredFullData] = useState([])
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        axios.post("https://backend-zjgv.onrender.com/api/filter", { lcost: formData.lcost, location: formData.location, hcost: formData.hcost, cuisine: formData.cuisine, mealtype: mealName, sort: formData.sort, page: formData.page }).then((res) => setFilteredData(res.data.result))
+        axios.post("https://backend-zjgv.onrender.com/api/filter", { lcost: formData.lcost, location: formData.location, hcost: formData.hcost, cuisine: formData.cuisine, mealtype: mealName, sort: formData.sort, page: formData.page }).then((res) => setFilteredFullData(res.data.full))
+    }, [formData, mealName])
+
+    if (filteredFullData.length) {
+        let num = filteredFullData.length
+        var half = Math.ceil(num / 2)
+    }
+    const pageHandler = (e) => {
+        const num = e.target.innerHTML
+        const pageNumber = parseInt(num)
+        setFormData({ ...formData, page: pageNumber })
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <div className='px-5 py-4'>
+                <h3 className='fs-1 fw-bold' style={{ color: "#192F60" }}><span>{mealName.charAt(0).toUpperCase()}{mealName.slice(1)}</span> Places in India</h3>
+                <div className='d-flex gap-4 mt-4' style={{ height: "40rem" }}>
+                    <div className='h-full col-3 rounded shadowx p-2'>
+                        <Filters />
+                    </div>
+                    <div className='col-9 d-flex flex-column justify-content-between gap-4'>
+                        {
+                            filteredData ?
+                                filteredData.map((item, i) => {
+                                    return <div key={i} className='rounded shadowx p-4' role='button' onClick={() => { setNavid(item._id); navigate(`/restaurant/${item._id}`) }}>
+                                        <div className='mb-4 d-flex gap-4 items-center'>
+                                            <img src={item.thumb} alt="imge" className='h-[6.5rem] w-[6.5rem] rounded' style={{ height: "6.5rem", width: "6.5rem" }} />
+                                            <div style={{ color: "#192F60" }}>
+                                                <h3 className=' fw-semibold '>{item.name}</h3>
+                                                <span className=' py-1 d-inline-block'>Fort</span>
+                                                <span className='text-sm d-block' style={{ color: "#636F88" }}>{item.address}</span>
+                                            </div>
+                                        </div>
+                                        <hr />
+                                        <div className='mt-4 d-flex gap-4'>
+                                            <div className='d-flex flex-column gap-1'>
+                                                <div>CUISINES:</div>
+                                                <div>COST FOR TWO:</div>
+                                            </div>
+                                            <div className='d-flex flex-column  gap-1'>
+                                                <div>
+                                                    <span>{item.Cuisine[0].name}, </span>
+                                                    <span>{item.Cuisine[1].name}</span>
+                                                </div>
+                                                <p>{item.cost}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                }) : "8"
+                        }
+                        <div className='justify-self-bottom text-center p-2' >
+                            <span className='border px-3 py-2 mx-2 ' role='button' > &#60;</span>
+                            {
+                                Array.from({ length: half }, (k, index) => {
+                                    return <span key={index} className='border px-3 py-2 mx-2' role='button' value={index + 1} onClick={pageHandler}>{index + 1}</span>
+                                })
+                            }
+                            <span className='border px-3 py-2 mx-2' role='button'> &#62;</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MealType
---
+++ src/pages/ResDetails/ResDetails.jsx
@@ -0,0 +1,74 @@
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
+import { store } from '../../App'
+import Navbar from '../../components/Navbar/Navbar'
+import CartProductModal from '../../components/UserModal/CartModal/CartProductModal'
+import AddressModal from '../../components/UserModal/CartModal/AddressModal'
+import PaymentModal from '../../components/UserModal/CartModal/PaymentModal'
+
+const ResDetails = () => {
+    const [resData, setResData] = useState({})
+    const [isOverViewClicked, setIsOverViewClicked] = useState(true)
+    const [isContactClicked, setIsContactClicked] = useState(false)
+    const { navId } = useContext(store)
+
+    useEffect(() => {
+        axios.get(`https://backend-zjgv.onrender.com/api/restaurant/${navId}`).then((result) => setResData(result.data))
+    }, [navId])
+
+    return (
+        <div>
+            <Navbar />
+            {
+                resData &&
+                <div className='py-5 ' style={{ height: "50rem", padding: "0 5rem" }}>
+                    <img src={resData.thumb} alt="hh" className='h-[25rem] w-100' style={{ height: "25rem", objectFit: "cover" }} />
+                    <h4 className=' fs-2  mt-4' style={{ color: "#192F60" }}>{resData.name}</h4>
+                    <div className='mt-2 py-2 d-flex justify-content-between'>
+                        <div style={{ color: "#192F60" }}>
+                            <span className={` fs-5 px-4 border-bottom ${isOverViewClicked ? "border-danger" : ""} py-2 me-3 cursor-pointer`} onClick={() => { setIsContactClicked(false); setIsOverViewClicked(true) }} role='button'>Overview</span>
+                            <span className={` fs-5 px-4 border-bottom py-2 me-3 cursor-pointer ${isContactClicked ? "border-danger" : ""}`} onClick={() => { setIsOverViewClicked(false); setIsContactClicked(true) }} role='button'>Contact</span>
+                        </div>
+                        <p className=' py-2 px-4 d-inline text-white' style={{ marginTop: "-.5rem", background: "#CE0505" }} data-bs-toggle="modal" data-bs-target="#cartProductModal">Place Online Order</p>
+                    </div>
+                    <div className='mb-3'>
+                        {
+                            isOverViewClicked &&
+                            <>
+                                <div className=' fs-5 fw-bold mt-4' style={{ color: "#192F60" }}>About this place</div>
+                                <div className=' fs-6 fw-semibold mt-3' style={{ color: "#192F60" }}>Cuisine</div>
+                                <div className='fs-6' style={{ color: "#192F60" }}>
+                                    {
+                                        resData.Cuisine &&
+                                        resData.Cuisine.map((i, index) => {
+                                            return <span key={index} className='pe-2'>{i.name}</span>
+                                        })
+                                    }
+                                </div>
+                                <div className='fs-6 fw-semibold mt-3' style={{ color: "#192F60" }}>Average Cost</div>
+                                <div className='mt-1 fs-6 mb-5' style={{ color: "#192F60" }}>
+                                    {
+                                        <span>₹{resData.cost} for two people (approx.)</span>
+                                    }
+                                </div>
+                            </>
+                        }
+                        {
+                            isContactClicked &&
+                            <>
+                                <div className='fs-6  mt-3' style={{ color: "#192F60" }}>Phone Number</div>
+                                <div className=''>{resData.contact_number}</div>
+                                <div className=' fw-semibold  mt-4' style={{ color: "#192F60" }}>{resData.name}</div>
+                                <div className='mt-1 mb-6'>{resData.address}</div>
+                            </>
+                        }
+                    </div>
+                </div>}
+            <CartProductModal resData={resData} />
+            <AddressModal data={resData} />
+            <PaymentModal />
+        </div >
+    )
+}
+
+export default ResDetails
